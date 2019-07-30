53ad5dd526fd021fe2c07b1df1525f1745368987
freez.fang <freez.fang@friendsmore.cn>
2019-07-30 16:29:16 +0800

    优化消息解析逻辑

--- src/Service/Notify/Client.php
+++ src/Service/Notify/Client.php
@@ -5,42 +5,26 @@ namespace LaravelWechatpayV3\Service\Notify;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Config;
 use LaravelWechatpayV3\Kernel\BaseClient;
-use LaravelWechatpayV3\Kernel\Exceptions\SignInvalidException;
 use LaravelWechatpayV3\Kernel\Utils\AesUtil;
-use Psr\Http\Message\ResponseInterface;
 
 /**
  * Class Client.
  */
 class Client extends BaseClient
 {
-
     /**
-     * @param ResponseInterface $response
+     * @param array $resource
      * @return array|mixed
-     * @throws SignInvalidException
+     * @throws \LaravelWechatpayV3\Kernel\Exceptions\InvalidArgumentException
+     * @throws \LaravelWechatpayV3\Kernel\Exceptions\RuntimeException
      */
-    public function parseResponse(ResponseInterface $response)
+    public function parseResource(array $resource)
     {
-        if (!$this->isResponseSignValid($response)) {
-            throw new SignInvalidException('响应验签失败');
-        }
+        $aesKey = Config::get('wechatpay-v3.aes_key', '');
+        $associatedData = Arr::get($resource, 'associated_data');
+        $nonceStr = Arr::get($resource, 'nonce');
+        $cipherText = Arr::get($resource, 'ciphertext');
 
-        $body = json_decode($response->getBody()->getContents());
-
-        if (JSON_ERROR_NONE !== json_last_error()) {
-            return [];
-        }
-
-        Arr::set($body, 'data', function () use ($body) {
-            $aesKey = Config::get('wechatpay-v3.aes_key', '');
-            $associatedData = Arr::get($body, 'resource.associated_data');
-            $nonceStr = Arr::get($body, 'resource.nonce');
-            $cipherText = Arr::get($body, 'resource.ciphertext');
-
-            return (new AesUtil($aesKey))->decryptAES256GCM($associatedData, $nonceStr, $cipherText);
-        });
-
-        return $body;
+        return (new AesUtil($aesKey))->decryptAES256GCM($associatedData, $nonceStr, $cipherText);
     }
 }
